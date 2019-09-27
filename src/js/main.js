1e37289fa039a0d1e2e9175cb5e59fa9e46ba74a
Kamil Baczynski <kamil-baczynski1@wp.pl>
2019-09-27 21:12:49 +0200

    Feat: Downloading list of current repositories by Fetch API

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,36 @@
 "use strict";
 
-console.log(`Hello world!`);
+const projectsList = document.querySelector('.projects__containter');
+
+fetch ('https://api.github.com/users/kamilbaczynski/repos?sort=updated')
+  .then (response => response.json())
+  .then (response => {
+    const repos = response;
+    for (const repo of repos) {
+      // console.log(repo)
+      // console.log(`${repo.name} ${repo.html_url}`);
+
+      projectsList.innerHTML += `<article class="project-card">
+      <img class="project-card__image" src="../assets/img/github42.svg" alt="#">
+      <h3 class="project-card__name">${repo.name}</h3>
+      <p class="project-card__description">${repo.description}</p>
+      <span class="project-card__footer">
+        <a class="project-card__footer--link project-card__footer--link-demo" href="${repo.homepage}">demo</a>
+        <a class="project-card__footer--link project-card__footer--link-github" href="${repo.html_url}">github</a>
+      </span>
+      </article>    
+      `
+    }
+  });
+  
 
 
+  // <article class="project-card">
+  // <img class="project-card__image" src="../assets/img/github42.svg" alt="#">
+  // <h3 class="project-card__name">title</h3>
+  // <p class="project-card__description">opis</p>
+  // <span class="project-card__footer">
+  //   <a class="project-card__footer--link project-card__footer--link-demo" href="#">demo</a>
+  //   <a class="project-card__footer--link project-card__footer--link-github" href="#">github</a>
+  // </span>
+  // </article>
