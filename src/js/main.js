cbf50567af19b8efab6271c04f89b5998fbaaf3d
Kamil Baczynski <kamil-baczynski1@wp.pl>
2019-10-02 18:35:57 +0200

    Feat: Adding target, rel, title values to links

--- src/js/main.js
+++ src/js/main.js
@@ -11,29 +11,27 @@ fetch ('https://api.github.com/users/kamilbaczynski/repos?sort=created')
       // console.log(`${repo.name} ${repo.html_url}`);
       const {description, homepage, html_url, name} = repo;
 
-      projectsList.innerHTML += `<article class="project-card">
-      <img class="project-card__image" src="../assets/img/github42.svg" alt="#">
-      <h3 class="project-card__name">${name}</h3>
-      ${
-        description 
-        ? `<p class="project-card__description">${description}</p>` 
-        : `<p class="project-card__description">No description 😫</p>`
-      }
-      <div class="project-card__footer">
-
+      projectsList.innerHTML +=`
+      <article class="project-card">
+        <img class="project-card__image" src="../assets/img/github42.svg" alt="#">
+        <h3 class="project-card__name">${name}</h3>
         ${
-          homepage 
-          ? `<a class="project-card__footer--link project-card__footer--link-demo" href="${homepage}">demo</a>` 
-          : `<p class="project-card__footer--link project-card__footer--link-disactive">demo</p>`
+          description 
+          ? `<p class="project-card__description">${description}</p>` 
+          : `<p class="project-card__description">No description 😫</p>`
         }
+        <div class="project-card__footer">
 
-        ${
-          html_url 
-          ? `<a class="project-card__footer--link project-card__footer--link-github" href="${html_url}">github</a>` 
-          : `<a class="project-card__footer--link project-card__footer--link-github" href="">github</a>`
-        }
+          ${
+            homepage 
+            ? `<a class="project-card__footer--link project-card__footer--link-demo" href="${homepage}" target="_blank" rel="nofollow noreferrer"  title="Demo: ${name}">demo</a>` 
+            : ''
+          }
+        
+          <a class="project-card__footer--link project-card__footer--link-github" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}">github</a> 
+          
       
-      </div>
+        </div>
         
       </article>    
       `
